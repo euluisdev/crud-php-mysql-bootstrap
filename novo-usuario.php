7aea34d83483e0128881348b45b0ee8b984429c4
euluis-dev <fluisf00@gmail.com>
2024-02-27 16:33:02 -0300

    created form in file novo-usuario.php

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,1 +1,22 @@
 <h1>Novo Usuário</h1>
+<form action="?page=salvar" method="POST"> <!-- linha para page=salvar form com POST -->
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="password" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Data de Nascimento</label>
+        <input type="date" name="date-nasc" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class=""btn btn-primary>Enviar</button>
+    </div>
+</form>
