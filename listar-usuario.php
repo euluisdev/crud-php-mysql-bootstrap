d9773a35eee59b631f3ba995b65bbff3fee93957
euluis-dev <fluisf00@gmail.com>
2024-02-28 00:41:30 -0300

    the listar-usuario.php file was created and the data shown on screen

--- listar-usuario.php
+++ listar-usuario.php
@@ -1,1 +1,20 @@
 <h1>Listar Usuário</h1>
+<?php 
+    $sql = "SELECT * FROM usuarios";
+
+    $res = $conn->query($sql);
+
+    $qtd = $res->num_rows;
+
+    if($qtd > 0) {
+        while($row = $res->fetch_object()) {
+            print $row->id;
+            print $row->nome;
+            print $row->email;
+            print $row->data_nasc;
+        }
+    }else{
+        print "<p class='alert alert-danger'>Não encontrou resultados!</p>";
+    }
+
+?>
