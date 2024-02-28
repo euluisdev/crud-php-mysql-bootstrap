ea46d24766813800a4f7c498ec5727b52d2aff86
euluis-dev <fluisf00@gmail.com>
2024-02-28 00:20:51 -0300

    finalized case(cadastrar) in file salvar-usuario.php-conexao db ok

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -9,6 +9,14 @@
             $sql = "INSERT INTO usuarios (nome, email, senha, data_nasc) VALUES ('{$nome}', '{$email}', '{$senha}', '{$data_nasc}')";
             
             $res = $conn->query($sql);
+
+            if($res == true) {
+                print "<script>alert('Cadastrado com sucesso!');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else{
+                print "<script>alert('Não foi possível cadastrar');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
             break;
         case 'editar':
             
