22ba1c1beca8c79c43f31532a6936b0be510f034
euluis-dev <fluisf00@gmail.com>
2024-02-29 00:54:24 -0300

    was created button excluir in the listar-usuario.php.file

--- listar-usuario.php
+++ listar-usuario.php
@@ -22,8 +22,8 @@
             print "<td>".$row->email."</td>";
             print "<td>".$row->data_nasc."</td>";
             print "<td>
-                    <button onclick=\"location.href='?page=editar&id=".$row->id."'\" class='btn btn-success'>Editar</button>
-                    <button class='btn btn-danger'>Excluir</button>
+                    <button onclick=\"location.href='?page=editar&id=".$row->id."';\" class='btn btn-success'>Editar</button>
+                    <button onclick=\"if(confirm('Tem certeza que deseja excluir?')){location.href='?page=salvar&acao=excluir&id=".$row->id."';}else{false;}\" class='btn btn-danger'>Excluir</button>
                    </td>";
             print "</tr>";
         }
