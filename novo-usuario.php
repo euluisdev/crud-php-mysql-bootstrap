89d0319fab9915cd0d2ed5c17741d63c26e66800
euluis-dev <fluisf00@gmail.com>
2024-03-04 11:31:42 -0300

    the layout of the novo-usuario.php file was changed, and gender was included

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,23 +1,35 @@
-<h1>Novo Usuário</h1>
-<form action="?page=salvar" method="POST"> <!-- linha para page=salvar form com POST -->
-    <input type="hidden" name="acao" value="cadastrar"> <!-- a ação vai oculta, não vê a url -->
-    <div class="mb-3">
-        <label>Nome</label>
-        <input type="text" name="nome" class="form-control">
+<h1 class="mb-5">Cadastrar Usuários</h1>
+
+<form action="?page=salvar" method="POST">  <!-- linha para page=salvar form com POST -->
+    <input type="hidden" name="acao" value="cadastrar">  <!-- a ação vai oculta, não vê a url -->
+    <div class="form-floating mb-3">
+        <input type="text" name="nome" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+        <label for="floatingInput">Digite seu nome</label>
+    </div>
+    <label class="mb-1">Sexo</label>
+    <div class="form-floating mb-3">
+        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-secondary active">
+                <input type="radio" name="genero" value="masculino" id="option1" autocomplete="off" required> Masculino
+            </label>
+            <label class="btn btn-secondary">
+                <input type="radio" name="genero" value="feminino" id="option2" autocomplete="off" required> Feminino
+            </label>
+        </div>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="email" name="email" class="form-control" placeholder="email@example.com" required>
+        <label for="floatingInput">Seu melhor Email</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="password" name="senha" class="form-control" placeholder="Password" required>
+        <label for="floatingPassword">Digite sua senha</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="date" name="data_nasc" class="form-control" required>
+        <label for="floatingInput">Data de nascimento</label>
     </div>
     <div class="mb-3">
-        <label>Email</label>
-        <input type="email" name="email" class="form-control">
+        <button type="submit" class="btn btn-intline-dark">Enviar</button>
     </div>
-    <div class="mb-3">
-        <label>Senha</label>
-        <input type="password" name="senha" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label>Data de Nascimento</label>
-        <input type="date" name="data_nasc" class="form-control">
-    </div>
-    <div class="mb-3">
-        <button type="submit" class=""btn btn-primary>Enviar</button>
-    </div>
-</form>
+</form> 
